2da70d530e55afb4043dac67890fd7be759168cd
shree <shree@shree-PC>
2022-03-24 22:54:20 +0530

    First Commit

---
+++ src/test/java/TestYantra/GITPracticeProject/Sample1Test.java
@@ -0,0 +1,12 @@
+package TestYantra.GITPracticeProject;
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+	@Test
+	public void sample()
+	{
+		System.out.println("Sample class created");
+	}
+
+}
---
+++ src/test/java/TestYantra/GITPracticeProject/Sample2Test.java
@@ -0,0 +1,12 @@
+package TestYantra.GITPracticeProject;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test {
+	@Test
+	public void sample2()
+	{
+		System.out.println("sample 2 class created");
+	}
+
+}
